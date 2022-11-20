b931e2799eb2ed2f55b066bb288b77346a6e287d
Alam-Ashraf <alamgeer06@gmail.com>
2022-11-20 23:40:52 +0530

    Initial commit

---
+++ XFHandleViewOnOrientationChangeSample/XFHandleViewOnOrientationChangeSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFHandleViewOnOrientationChangeSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width > height)
+            {
+                // Landscape
+                VisualStateManager.GoToState(FlexLayout, "Landscape");
+            }
+            else
+            {
+                // Portrait
+                VisualStateManager.GoToState(FlexLayout, "Portrait");
+            }
+        }
+    }
+}
+
